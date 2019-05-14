f037e04ef284c13a4da91a1c6600694cf1e5ee87
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2019-05-14 10:37:02 +0200

    rlq → rql

--- dump_rql_upload/dump_rql_upload.sh
+++ dump_rql_upload/dump_rql_upload.sh
@@ -3,4 +3,4 @@
 DIRNAME=`dirname $0`
 
 . ~/virtualenvs/cveda_upload/bin/activate
-cubicweb-ctl shell cveda_upload "$DIRNAME/dump_rql_upload.py" > /cveda/chroot/data/tmp/cati/dump_rlq_upload.json
+cubicweb-ctl shell cveda_upload "$DIRNAME/dump_rql_upload.py" > /cveda/chroot/data/tmp/cati/dump_rql_upload.json
