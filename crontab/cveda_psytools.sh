84f59f52025ab33ac84db01c2b56a693bb931a3d
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2019-07-17 18:17:02 +0200

    The new derivation functions need precise timing
    This means derivation must happen before de-identification as de-deidentification replaces precise timestamps with the age in da
    ys.

--- crontab/cveda_psytools.sh
+++ crontab/cveda_psytools.sh
@@ -3,11 +3,9 @@
 # this script is executed daily by databank@cveda.nimhans.ac.in at 04:10 AM IST
 #
 # 1. It downloads data from the Delosis server into raw CSV files.
-# 2. It pseudonynmizes the raw CSV files.
-# 3. It derives the pseudoymized CSV files.
-# 4. It temporarily derives the raw CSV files, for QC purposes only.
+# 2. It derives the raw CSV files and makes them available for QC purposes.
+# 3. It pseudonynmizes the derived CSV files.
 
 /cveda/databank/framework/cveda_databank/psytools/cveda_psytools_download.py 2>&1 | ts '%Y-%m-%d %H:%M:%S %Z' >> /var/log/databank/cveda_psytools_download.log
-/cveda/databank/framework/cveda_databank/psytools/cveda_psytools_deidentify.py 2>&1 | ts '%Y-%m-%d %H:%M:%S %Z' >> /var/log/databank/cveda_psytools_deidentify.log
 /cveda/databank/framework/cveda_r/R/cveda_psytools_derive.R 2>&1 | ts '%Y-%m-%d %H:%M:%S %Z' >> /var/log/databank/cveda_psytools_derive.log
-/cveda/databank/framework/tmp/cveda_psytools_derive.R
+/cveda/databank/framework/cveda_databank/psytools/cveda_psytools_deidentify.py 2>&1 | ts '%Y-%m-%d %H:%M:%S %Z' >> /var/log/databank/cveda_psytools_deidentify.log
